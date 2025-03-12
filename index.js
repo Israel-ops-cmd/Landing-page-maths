631c016fbc4b6f08fd67fc6c54566d4a982e14af
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-12 07:54:49 -0300

    Adicionando o link ao módulo I

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const botaoAbrir = document.querySelector("#pagamento button");
     const botaoFechar = document.querySelector("#fechar");
     const divCompra = document.querySelector("#compra");
+    const botaoModulo1 = document.querySelector("#ebooks button:first-child"); 
 
     botaoAbrir.addEventListener("click", function () {
         divCompra.classList.remove("hidden");
@@ -9,5 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     botaoFechar.addEventListener("click", function () {
         divCompra.classList.add("hidden");
+    });
+
+    botaoModulo1.addEventListener("click", function () {
+        window.open("https://pay.herospark.com/matematica-do-zero-modulo-i-409642", "_blank");
     });
 });
