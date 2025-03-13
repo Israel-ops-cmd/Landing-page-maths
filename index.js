a1911a43d606e78e6925b72b77291d79e92afa68
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-13 13:57:00 -0300

    Adicionando interface ao módulo 2

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ document.addEventListener("DOMContentLoaded", function () {
     const botaoAbrir = document.querySelector("#pagamento button");
     const botaoFechar = document.querySelector("#fechar");
     const divCompra = document.querySelector("#compra");
-    const botaoModulo1 = document.querySelector("#ebooks button:first-child"); 
+
+    const botaoModulo1 = document.querySelector("#ebooks button:first-child");
+    const botaoModulo2 = document.querySelector("#ebooks button:last-child");
+
+    const modalModulo2 = document.querySelector("#modal-modulo2");
+    const fecharModulo2 = document.querySelector("#fechar-modulo2");
+    const overlay = document.querySelector("#overlay");
 
     botaoAbrir.addEventListener("click", function () {
         divCompra.classList.remove("hidden");
-    });
+    })
 
     botaoFechar.addEventListener("click", function () {
         divCompra.classList.add("hidden");
-    });
+    })
 
     botaoModulo1.addEventListener("click", function () {
         window.open("https://adriel-lucas.herospark.co/matematica-do-zero-modulo-i", "_blank");
-    });
-});
+    })
+
+    botaoModulo2.addEventListener("click", function () {
+        modalModulo2.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    })
+
+    fecharModulo2.addEventListener("click", function () {
+        modalModulo2.classList.add("hidden");
+        overlay.classList.add("hidden");
+    })
+})
