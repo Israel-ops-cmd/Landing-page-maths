66d90399dc5d8031580d931206fe7f9b01980ea8
Israel-ops-cmd <israellipe2020@gmail.com>
2025-03-25 08:46:19 -0300

    retirando a div compra e fazendo os botões direcionarem direto para o link de pagamento

--- index.js
+++ index.js
@@ -1,33 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const botaoAbrir = document.querySelector("#pagamento button");
-    const botaoFechar = document.querySelector("#fechar");
-    const divCompra = document.querySelector("#compra");
-
-    const botaoModulo1 = document.querySelector("#ebooks button:first-child");
-    
-    const botaoBonus = document.querySelector("#bonus-btn"); 
-    const botaoGarantia = document.querySelector("#garantia button");
-
-
-    const botaoModuloSumario = document.querySelector("#modulo1 .mod");
-    botaoModuloSumario.addEventListener("click", function () {
-        divCompra.classList.remove("hidden");
-    })
-
-    botaoAbrir.addEventListener("click", function () {
-        divCompra.classList.remove("hidden");
-    })
-
-    botaoFechar.addEventListener("click", function () {
-        divCompra.classList.add("hidden");
-    })
-
-    botaoModulo1.addEventListener("click", function () {
-        window.open("https://pay.herospark.com/matematica-do-zero-modulo-i-410186");
-    })
-
     const faqQuestions = document.querySelectorAll('.faq-question');
-
+    
     faqQuestions.forEach(question => {
         question.addEventListener('click', () => {
             const answer = question.nextElementSibling;
@@ -40,14 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     })
 
-    const abrirCompra = function() {
-        divCompra.classList.remove("hidden");
-    }
-
-    botaoBonus.addEventListener("click", abrirCompra);
-    botaoGarantia.addEventListener("click", abrirCompra);
-
-
     function scrollToPayment() {
         const pagamentoSection = document.getElementById('pagamento');
         pagamentoSection.scrollIntoView({ 
@@ -55,7 +20,6 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 
-    
     const botaoNovaTurma = document.querySelector('button[type="button"]'); 
     if (botaoNovaTurma) {
         botaoNovaTurma.addEventListener("click", scrollToPayment);
